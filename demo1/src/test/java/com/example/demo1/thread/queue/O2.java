845d31b13736fd6a5965ec7a3995ca502e1f7fac
qiujie.gao@perfma.com <qiujie.gao@perfma.com>
2022-05-09 10:47:05 +0800

    队列的使用trandfer,DelayQueue

--- demo1/src/test/java/com/example/demo1/thread/queue/O2.java
+++ demo1/src/test/java/com/example/demo1/thread/queue/O2.java
@@ -38,6 +38,7 @@ public class O2 {
                     queue.tryTransfer(new c1("任务" + i),1000, TimeUnit.MILLISECONDS);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
+
                 }
                 System.out.println("放入成功了但是没有人取走，看看线程会不会继续执行，如果打印了说明线程没有等待结果取走");
                 i++;
